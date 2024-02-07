327e85e7f0f27eb2631a14eb3e5ab531ede6351a
ddyural <84669233+ddyural@users.noreply.github.com>
2024-02-07 21:30:24 +0300

    неудачно применил ограничение на вращение

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,15 @@ public class PlayerController : MonoBehaviour
     private float speed = 5f;
 
     [SerializeField]
-    private float lookSpeed = 100f;
+    private float lookSpeed = 3f;
+
+    [SerializeField]
+    private float minAngle = -90f; // Ограничение на вращение вверх
+
+    [SerializeField]
+    private float maxAngle = 90f; // Ограничение на вращение вниз
+
+    private float xRot = 0f; // Переменная понадобится в дальнейшем.
 
     private PlayerMotor motor;
 
@@ -28,9 +36,21 @@ public class PlayerController : MonoBehaviour
 
         motor.Move (velocity);
 
+
+
         float yRot = Input.GetAxisRaw("Mouse X"); // y rotation 
         Vector3 rotation = new Vector3(0f, yRot, 0f) * lookSpeed;
 
         motor.Rotate(rotation);
+
+
+
+        float xRot = Input.GetAxisRaw("Mouse Y") * lookSpeed; // Умножаем на lookSpeed здесь, чтобы учесть скорость вращения
+
+        xRot = Mathf.Clamp(xRot, minAngle, maxAngle); // Применяем ограничение на вращение вверх и вниз
+
+        Vector3 camRotation = new Vector3(xRot, 0f, 0f);
+
+        motor.RotateCam(camRotation);
     }
 }
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMotor : MonoBehaviour
 {
+    [SerializeField]
+    private Camera cam;
+
     private Rigidbody rb;
 
     private Vector3 velocity = Vector3.zero; // передвижение 
     private Vector3 rotation = Vector3.zero; // вращение    
+    private Vector3 rotationCamera = Vector3.zero; // вращение    
 
     void Start ()
     {
@@ -18,10 +22,16 @@ public class PlayerMotor : MonoBehaviour
         velocity = _velocity;
     }
 
-    public void Rotate (Vector3 _rotation)
+    public void Rotate (Vector3 _rotationCam)
+    {
+        rotationCamera = _rotationCam;
+    }
+
+    public void RotateCam (Vector3 _rotation)
     {
         rotation = _rotation;
     }
+
 
     void FixedUpdate ()
     {
@@ -40,6 +50,14 @@ public class PlayerMotor : MonoBehaviour
     void PerformRotation ()
     {
         rb.MoveRotation(rb.rotation * Quaternion.Euler (rotation) ); // rb.rotation - вращение, которое стоит сейчас
-    } // просто так вращать нельзя, нужно всё делать через Quaternion 
+        // просто так вращать нельзя, нужно всё делать через Quaternion
+        if (cam != null)
+        {
+            //cam.transform.localEulerAngles = -rotationCamera; // Применяем вращение к объекту(камере)
+            cam.transform.Rotate(-rotationCamera);
+        }
+    
+    
+    }  
 
 }
