256139accf5b72b268db8d941536aacb8abcbacc
ddyural <84669233+ddyural@users.noreply.github.com>
2024-02-16 21:46:06 +0300

    +ProgressBar

---
+++ Assets/Scripts/ProgressBarController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ProgressBarController : MonoBehaviour
+{
+    [Header("UI Elements")]
+    [SerializeField] private Image image;
+
+    [Header("Properties")]
+    [SerializeField] private int value;
+    [SerializeField] private int maxValue;
+    [SerializeField] private bool isCorrectlyConfigured;
+
+    private void Awake()
+    {
+        if (image.type == Image.Type.Filled & image.fillMethod == Image.FillMethod.Horizontal)
+        {
+            isCorrectlyConfigured = true;
+        }
+        else
+        {
+            Debug.Log(message: "No okay");
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (!isCorrectlyConfigured) return;
+        image.fillAmount = (float) value / maxValue;
+    }
+
+    public void SetValue(int value) => this.value = value;
+    public void SetMaxValue(int maxValue) => this.maxValue = maxValue;
+
+}
